6e70695c006dd0620e8e0172d8aac70af7051f7f
Ahmed Fathy <am.fathy396@gmail.com>
2020-05-22 01:54:41 +0200

    Initial commit

---
+++ Assets/_Scripts/Rotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		//transform.Rotate(0,20*Time.deltaTime,0);
+		transform.RotateAround (Vector3.up, 1 * Time.deltaTime);
+	}
+}
